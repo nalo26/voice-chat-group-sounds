5f95a02bcaa22e3fc4b39e6de5859bc9bc4fed0c
nalo26 <nath.v26@gmail.com>
2023-05-14 10:03:38 +0200

    :sparkles: New API

--- src/main/java/fr/nalo/voicechatgroupsounds/GroupSoundsVoiceChatPlugin.java
+++ src/main/java/fr/nalo/voicechatgroupsounds/GroupSoundsVoiceChatPlugin.java
@@ -1,14 +1,23 @@
 package fr.nalo.voicechatgroupsounds;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import de.maxhenkel.voicechat.api.Group;
 import de.maxhenkel.voicechat.api.VoicechatApi;
 import de.maxhenkel.voicechat.api.VoicechatPlugin;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.JoinGroupEvent;
 import de.maxhenkel.voicechat.api.events.LeaveGroupEvent;
+import de.maxhenkel.voicechat.api.events.PlayerStateChangedEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 
 public class GroupSoundsVoiceChatPlugin implements VoicechatPlugin {
+
+    private Group group;
+    private Set<UUID> players_in_group = new HashSet<>();
 
     /**
      * @return the unique ID for this voice chat plugin
@@ -25,7 +34,7 @@ public class GroupSoundsVoiceChatPlugin implements VoicechatPlugin {
      */
     @Override
     public void initialize(VoicechatApi api) {
-        // GroupSounds.LOGGER.info("Group Sounds voice chat plugin initialized!");
+        GroupSounds.LOGGER.info("Group Sounds voice chat plugin initialized!");
     }
 
     /**
@@ -37,20 +46,45 @@ public class GroupSoundsVoiceChatPlugin implements VoicechatPlugin {
     public void registerEvents(EventRegistration registration) {
         registration.registerEvent(JoinGroupEvent.class, this::onJoinGroupEvent);
         registration.registerEvent(LeaveGroupEvent.class, this::onLeaveGroupEvent);
+        registration.registerEvent(PlayerStateChangedEvent.class, this::onPlayerStateChangedEvent);
     }
 
     public void onJoinGroupEvent(JoinGroupEvent event) {
-        GroupSounds.LOGGER.info("User join group");
-        Minecraft minecraft = Minecraft.getInstance();
-        LocalPlayer player = minecraft.player;
-        player.playSound(GroupSounds.soundJoinGroupEvent);
+        GroupSounds.LOGGER.info("Join group event");
     }
 
     public void onLeaveGroupEvent(LeaveGroupEvent event) {
-        GroupSounds.LOGGER.info("User leave group");
+        GroupSounds.LOGGER.info("Leave group event");
+    }
+
+    public void onPlayerStateChangedEvent(PlayerStateChangedEvent event) {
+        GroupSounds.LOGGER.info("Player state changed event");
+        if (this.group == null || event.getConnection() == null || event.isDisabled() == true)
+            return;
+
         Minecraft minecraft = Minecraft.getInstance();
         LocalPlayer player = minecraft.player;
-        player.playSound(GroupSounds.soundLeaveGroupEvent);
+
+        UUID player_uuid = event.getPlayerUuid();
+
+        if (player_uuid.equals(player.getUUID())) {
+            GroupSounds.LOGGER.info("User joined/leaved group");
+            this.group = event.getConnection().getGroup();
+            return;
+        }
+
+        if (this.players_in_group.contains(player_uuid) && event.getConnection().getGroup() == null) {
+            GroupSounds.LOGGER.info("Player leaved group");
+            this.players_in_group.remove(player_uuid);
+            player.playSound(GroupSounds.soundLeaveGroupEvent);
+
+        } else if (!this.players_in_group.contains(player_uuid)
+                && this.group.equals(event.getConnection().getGroup())) {
+            GroupSounds.LOGGER.info("Player joined group");
+            this.players_in_group.add(player_uuid);
+            player.playSound(GroupSounds.soundJoinGroupEvent);
+        }
+
     }
 
 }
